ee20a5d71ec57d7618d80fb97e941f118d3d25ee
Matthew Duffy <mattduff707@hotmail.com>
2021-08-28 11:04:35 -0500

    Implemented MDN Doc search feature
    Utilized controlled inputs to create a search url in MDN docs.
    Added search property to data object to make it trivial to add
    search to future bookmarks

--- src/components/bookmarks/BookmarksList.js
+++ src/components/bookmarks/BookmarksList.js
@@ -1,13 +1,15 @@
-import React from "react";
-import styled from "styled-components";
-import Bookmark from "./Bookmark";
-import bookmarksData from "../../data/bookmarksData";
+import React from 'react';
+import styled from 'styled-components';
+import Bookmark from './Bookmark';
+import bookmarksData from '../../data/bookmarksData';
 
 const BookmarksList = () => {
   return (
     <Wrapper>
       {bookmarksData.map((i) => {
-        return <Bookmark key={i.title} title={i.title} baseURL={i.baseURL} extensions={i.extensions} />;
+        return (
+          <Bookmark key={i.title} title={i.title} baseURL={i.baseURL} extensions={i.extensions} search={i.search} />
+        );
       })}
       {/* <Bookmark baseURL={"#"} extensions={["Arrays", "Objects", "Promises"]} title="MDN Web Docs: JS" />
       <Bookmark baseURL={"#"} extensions={false} title="W3 Icon Reference" /> */}
--- src/data/bookmarksData.js
+++ src/data/bookmarksData.js
@@ -1,22 +1,24 @@
 const data = [
   {
-    title: "MDN Docs: JS",
-    baseURL: "https://developer.mozilla.org/en-US/docs/Web/JavaScript",
+    title: 'MDN Docs',
+    baseURL: 'https://developer.mozilla.org/en-US/',
     extensions: [
-      { title: "Array", url: "/Reference/Global_Objects/Array" },
-      { title: "Object", url: "/Reference/Global_Objects/Object" },
-      { title: "Function", url: "/Reference/Global_Objects/Function" },
-      { title: "async", url: "/Reference/Statements/async_function" },
+      { title: 'JS: Array', url: 'docs/Web/JavaScript/Reference/Global_Objects/Array' },
+      { title: 'JS: Object', url: 'docs/Web/JavaScript/Reference/Global_Objects/Object' },
+      { title: 'JS: Function', url: 'docs/Web/JavaScript/Reference/Global_Objects/Function' },
+      { title: 'JS: async', url: 'docs/Web/JavaScript/Reference/Statements/async_function' },
     ],
+    search: {
+      searchURL: 'https://developer.mozilla.org/en-US/search?q=',
+    },
   },
-  { title: "Icon Reference", baseURL: "https://www.w3schools.com/icons/icons_reference.asp", extensions: false },
-  { title: "Google Fonts", baseURL: "https://fonts.google.com/", extensions: false },
+  { title: 'Icon Reference', baseURL: 'https://www.w3schools.com/icons/icons_reference.asp', extensions: false },
+  { title: 'Google Fonts', baseURL: 'https://fonts.google.com/', extensions: false },
   {
-    title: "WAI-ARIA",
-    baseURL: "https://www.w3.org/TR/wai-aria-practices/",
+    title: 'WAI-ARIA',
+    baseURL: 'https://www.w3.org/TR/wai-aria-practices/',
     extensions: false,
   },
-  //   { baseURL: "", extensions: [] },
 ];
 
 export default data;
