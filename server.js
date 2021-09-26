9031544240aec41cc068b817666b3eb89d4d7a2b
Matthew Duffy <mattduff707@hotmail.com>
2021-09-26 12:19:29 -0500

    Deploy front end

--- server.js
+++ server.js
@@ -1,7 +1,10 @@
 const express = require('express');
 const fs = require('fs');
 const app = express();
+const path = require('path')
 app.use(express.json());
+
+app.use(express.static(path.join(__dirname, 'client/build')))
 // app.use(express.urlencoded({ extended: false }));
 
 // let snippets = ['snippet 1', 'snippet 2', 'snippet 3'];
