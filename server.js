b4206574d5654cc4c2532bd0e89cf0a12b669ac4
Matthew Duffy <mattduff707@hotmail.com>
2021-09-26 12:52:23 -0500

    Added get script to server

--- server.js
+++ server.js
@@ -1,10 +1,13 @@
 const express = require('express');
 const fs = require('fs');
 const app = express();
-const path = require('path')
+const path = require('path');
 app.use(express.json());
 
-app.use(express.static(path.join(__dirname, 'client/build')))
+app.use(express.static(path.join(__dirname, 'client/build')));
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname + '/client/build/index.html'));
+});
 // app.use(express.urlencoded({ extended: false }));
 
 // let snippets = ['snippet 1', 'snippet 2', 'snippet 3'];
